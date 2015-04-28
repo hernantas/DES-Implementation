adc588ab1d9b3479b72d5e16de995701fee72cac
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2015-04-28 22:51:07 +0700

    Generate public key and private key

--- DESEncryption/RSA.cs
+++ DESEncryption/RSA.cs
@@ -48,19 +48,46 @@ namespace DESEncryption
             return (extendedEuclid(a, m).Item2.Item1 + m) % m;
         }
 
-        public string GenerateKey()
+        // Format: First X Digit represent e, Last 15 Digit represent phi
+        public string GeneratePublicKey()
         {
             ulong first = generator.GetPrime();
             ulong second = generator.GetPrime();
             ulong mult = (first * second);
             ulong phi =  (first - 1) * (second -1);
             ulong e = generator.GetCoPrime(phi);
+
+            string padding = "";
+            for (int i = 0; i < 10-e.ToString().Length; i++)
+            {
+                padding += "0";
+            }
+
+            //Console.WriteLine(padding + e.ToString() + " | " + phi.ToString());
+
+            // (e, phi)
+            return padding + e.ToString() + phi.ToString();
+
+            //Console.WriteLine("First: " + first.ToString() + " Second: " + second.ToString() +
+            //    " Multiplier: " + mult + " Q: " + phi + " CoPrime: " + e + " D: " + d);
+        }
+
+        public string GeneratePrivateKey(string publicKey)
+        {
+            string sE = publicKey.Substring(0, 10);
+            string sPhi = publicKey.Substring(10);
+            ulong e = Convert.ToUInt64(sE);
+            ulong phi = Convert.ToUInt64(sPhi);
             ulong d = modInverse(e, phi);
 
-            Console.WriteLine("First: " + first.ToString() + " Second: " + second.ToString() +
-                " Multiplier: " + mult + " Q: " + phi + " CoPrime: " + e + " D: " + d);
-            return "First: " + first.ToString() + " Second: " + second.ToString() +
-                " Multiplier: " + mult + " Q: " + phi + " CoPrime: " + e + " D: " + d;
+            string padding = "";
+            for (int i = 0; i < 20-d.ToString().Length; i++)
+            {
+                padding += "0";
+            }
+
+            // (d, phi)
+            return padding + d.ToString() + phi.ToString() ;
         }
     }
 }
